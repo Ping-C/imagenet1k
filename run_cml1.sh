beb4e9b61144f5ab83a6cb71394acf1716cff209
Ping <pchiang@umd.edu>
2022-09-22 16:47:51 -0400

    update experiment script for aws

--- run_cml1.sh
+++ run_cml1.sh
@@ -1,1 +1,2 @@
 python train_imagenet.py --config-file configs/simplevit/vits_1000cls_cml.yaml --training.altnorm=1 --lr.warmup_epochs=8 --logging.project_name imgnet1K_simplevits --logging.resume_id=aaa --logging.folder outputs/simplevits/baseline
+python train_imagenet.py --config-file configs/simplevit/vits_1000cls_cml.yaml --training.randaug_version=v4 --model.arch=vit_s_v8 --training.altnorm=1 --lr.warmup_epochs=8 --logging.project_name imgnet1K_simplevits --logging.resume_id=aab --logging.folder outputs/simplevits/baseline_v2
