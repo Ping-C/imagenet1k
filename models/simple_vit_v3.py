893c7a54cb042118f57514decbabfa8dc3f4fafe
Ping-yeh Chiang <pchiang@umd.edu>
2022-10-13 17:50:43 +0000

    preliminary draft of generator

--- models/simple_vit_v3.py
+++ models/simple_vit_v3.py
@@ -208,3 +208,106 @@ class SimpleViT_v3(nn.Module):
         for layer in self.transformer.layers[:layers_n]:
             for para in layer.parameters():
                 para.requires_grad = True
+
+                
+
+class PyramidGenerator(nn.Module):
+    def __init__(self, image_size, patch_size, dim, depth, heads, mlp_dim, channels = 3, dim_head = 64, probe=False):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b h w (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
+        )
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim)
+        self.encoder_norm = nn.LayerNorm(dim)
+
+        self.to_patch_0 = nn.Sequential(
+            nn.Linear(dim, patch_dim),
+            nn.Sigmoid(),
+            Rearrange('b (h w) (p1 p2 c) -> b c (h p1) (w p2)', p1 = patch_height, p2 = patch_width, 
+                      h=image_height // patch_height, 
+                      w=image_width // patch_width),
+        )
+        self.to_patch_1 = nn.Sequential(
+            nn.Linear(dim, patch_dim//(16*16)),
+            nn.Sigmoid(),
+            Rearrange('b (h w) (p1 p2 c) -> b c (h p1) (w p2)', p1 = patch_height//16, p2 = patch_width//16, 
+                      h=image_height // patch_height, 
+                      w=image_width // patch_height)
+        )
+        self.to_patch_2 = nn.Sequential(
+            nn.Linear(dim, patch_dim//(16*16)),
+            nn.Sigmoid(),
+            Rearrange('b (h w) (p1 p2 c) -> b c (h p1) (w p2)', p1 = patch_height//16, p2 = patch_width//16, 
+                      h=image_height // patch_height, 
+                      w=image_width // patch_height)
+        )
+        self._reset_parameters()
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            xavier_uniform_(module.weight.data)
+            if module.bias is not None:
+                module.bias.data.normal_(mean=0.0, std=1e-6)
+
+    def _linear_init(self, module):
+        fan_in = module.weight.shape[1]
+        stdev = torch.sqrt(torch.tensor(1/fan_in))/.87962566103423978
+        nn.init.trunc_normal_(module.weight, mean=0.0, std=1.0*stdev, a=- 2.0*stdev, b=2.0*stdev)
+        nn.init.zeros_(module.bias)
+
+    def _reset_parameters(self):
+        self.to_patch_0[0].apply(self._linear_init)
+        self.to_patch_1[0].apply(self._linear_init)
+        self.to_patch_2[0].apply(self._linear_init)
+        self.to_patch_embedding[1].apply(self._linear_init)
+
+        # reset attention
+        # reset mlp blocks
+        for module in self.modules():
+            if isinstance(module, FeedForward) or isinstance(module, Attention):
+                module._reset_parameters()
+        
+        for module in self.modules():
+            if isinstance(module, Attention):
+                fan_in = module.to_qkv.weight.shape[0]
+                fan_out = module.to_qkv.weight.shape[1]
+                modify_gain = torch.sqrt(torch.tensor((fan_in+fan_out)/(fan_in/3+fan_out)))
+                xavier_uniform_(module.to_qkv.weight, gain=modify_gain)
+        
+
+    def forward(self, img, feature_noise={}, get_features=False, get_linear_probes=False, freeze_layers=None, get_attention_maps=False):
+        *_, h, w, dtype = *img.shape, img.dtype
+
+        x = self.to_patch_embedding(img)
+        pe = posemb_sincos_2d(x)
+        x = rearrange(x, 'b ... d -> b (...) d') + pe
+        x = self.transformer(x, feature_noise=feature_noise, freeze_layers=freeze_layers)
+        x = self.encoder_norm(x)
+        x0 = self.to_patch_0(x)
+        x0 += torch.nn.functional.interpolate(self.to_patch_1(x), x0.shape[2:])*10
+        x_32 = torch.nn.functional.interpolate(self.to_patch_2(x), size=(7,7))
+        x0 += torch.nn.functional.interpolate(x_32, x0.shape[2:])*20
+        
+        return x0
+if __name__ == "__main__":
+    import pdb;pdb.set_trace()
+    generator = PyramidGenerator(image_size=224, 
+                                 patch_size=16, 
+                                 dim=384, 
+                                 depth=1, 
+                                 heads=6, 
+                                 mlp_dim=384*4)
+    images = torch.randn((5, 3, 224, 224))
+    out = generator(images)
+    pass
+    
